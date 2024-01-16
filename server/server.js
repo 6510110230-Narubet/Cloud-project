0bbc4ae9a69dff95a1187ed0c75a8ac118d5f1ca
6510110230-Narubet <bosschikung@gmail.com>
2024-01-16 13:56:49 +0700

    create server

---
+++ server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+
+app.post('/login', (req, res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId:'27e87247be3840bea2c12f0fabec2f03',
+        ClientSecret:'f4e988f8c6524ff4980e11eb9beedb73'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        res.json({
+            accessToken: data.body.access_token,
+            refreshToken: data.body.refresh_token,
+            expiresIn: data.body.expires_in
+        })
+    }).catch(() => {
+        res.sendStatus(400)
+    })
+})
