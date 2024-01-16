6fa1afb84113c3f87a01416194d9570b5eb75cc2
6510110230-Narubet <bosschikung@gmail.com>
2024-01-16 13:28:53 +0700

    Create Login page

--- music app/src/Login.js
+++ music app/src/Login.js
@@ -7,10 +7,22 @@ const AUTH_URL =
 
 export default function Login() {
   return (
-    <div>
-      <Button type="button" className="btn btn-success" href={AUTH_URL}>
-        Login with Spotify
-      </Button>
+    <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '100vh', background: '#474747 ' }}>
+      <div className="row">
+        <div className="col-12 text-center mb-3">
+          <img
+            src={require('./image/anyalogo.png')}
+            alt="Logo"
+            className="logo-img"
+            style={{ backgroundColor: 'white', width: '200px', height: '200px', borderRadius: '50%', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)' }}
+          />
+        </div>
+        <div className="col-12 text-center">
+          <Button type="button" className="btn btn-success" style={{ width: '200px', height: '60px' }} href={AUTH_URL}>
+            Login with Treeman Music
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
