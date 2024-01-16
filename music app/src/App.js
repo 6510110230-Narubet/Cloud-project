d862d1df152be0611665ecf3d5c01afafcd31252
6510110230-Narubet <bosschikung@gmail.com>
2024-01-16 14:08:02 +0700

    Create Dashboard

--- music app/src/App.js
+++ music app/src/App.js
@@ -1,8 +1,11 @@
 import Login from './Login';
+import Dashboard from './Dashboard';
 
+const code  = new URLSearchParams(window.location.search).get
+("code")
 
 function App() {
-  return <Login />
+  return code ? <Dashboard code={code} /> : <Login />
 }
 
 export default App;
